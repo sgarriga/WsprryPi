17d27289a867e3cd9d1ee89b1fa9eef153fa0cad
Stephen Garriga <steve_garriga@yahoo.com>
2023-10-05 12:20:02 -0400

    Finally! A good test

--- test-20231003.sh
+++ test-20231003.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# the TEST runs Oct 3 2023 from 1852hrs to 1854hrs UTC...
+# the TEST runs Oct 3 2023 from 2220hrs to 2250hrs UTC...
 # Let's start our beacon 1 hour before and shutdown 10 min after
 #
-local_time=`date --date='TZ="UTC" 2023-10-04 18:52:00' +%Y%m%d%H%M`
+local_time=`date --date='TZ="UTC" 2023-10-04 22:20:00' +%Y%m%d%H%M`
 echo "wspr_beacon.sh" | at -q z -t $local_time
 
-local_time=`date --date='TZ="UTC" 2023-10-04 18:54:00' +%Y%m%d%H%M`
+local_time=`date --date='TZ="UTC" 2023-10-04 22:50:00' +%Y%m%d%H%M`
 echo "sudo pkill -9 wspr" | at -q z -t $local_time
